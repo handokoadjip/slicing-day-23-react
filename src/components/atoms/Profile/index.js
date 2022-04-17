95025a0630bbbf838141704422d9d10f2d4da123
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-04-17 12:35:24 +0700

    revision - add tag figure to img

--- src/components/atoms/Profile/index.js
+++ src/components/atoms/Profile/index.js
@@ -4,11 +4,13 @@ import propTypes from "prop-types";
 const index = (props) => {
   return (
     <div className="wrapper mb-3">
-      <img
-        className="h-image-150px h-image-lg-300px w-100 grayscale mb-5"
-        src={props.image}
-        alt={props.name}
-      />
+      <figure className="m-0">
+        <img
+          className="h-image-150px h-image-lg-300px w-100 grayscale mb-5"
+          src={props.image}
+          alt={props.name}
+        />
+      </figure>
       <p class="text-fade-black-1 fs-5 pt-3">{props.quote}</p>
       <p class="text-fade-black-1 text-uppercase">{props.name}</p>
     </div>
